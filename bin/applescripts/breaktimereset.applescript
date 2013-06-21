c25b80a87a647e9a71af21fc3dbc99240c7c3066
Vladimir Rybas <vladimirrybas@gmail.com>
2013-06-21 09:00:41 +0700

    Reset Thyme and BreakTime simultaneously

---
+++ bin/applescripts/breaktimereset.applescript
@@ -0,0 +1,5 @@
+tell application "BreakTime"
+  set enabled to not enabled
+  set enabled to not enabled
+end tell
+
--- thyme/aliases.zsh
+++ thyme/aliases.zsh
@@ -1,3 +1,3 @@
-alias td='thyme -d'
-alias ts='thyme -s'
+alias td='thyme -d && osascript ~/.dotfiles/bin/applescripts/breaktimereset.applescript'
+alias ts='thyme -s && osascript ~/.dotfiles/bin/applescripts/breaktimereset.applescript'
 
