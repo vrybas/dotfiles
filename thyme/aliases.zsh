91cc203c9e36421c84cda3afdc7f4806ad8a650c
Vladimir Rybas <vladimirrybas@gmail.com>
2013-06-21 11:55:04 +0700

    Reset Thyme if already started

--- thyme/aliases.zsh
+++ thyme/aliases.zsh
@@ -1,3 +1,3 @@
-alias td='thyme -d && osascript ~/.dotfiles/bin/applescripts/breaktimereset.applescript'
+alias td='thyme -s && thyme -d && osascript ~/.dotfiles/bin/applescripts/breaktimereset.applescript'
 alias ts='thyme -s && osascript ~/.dotfiles/bin/applescripts/breaktimereset.applescript'
 
