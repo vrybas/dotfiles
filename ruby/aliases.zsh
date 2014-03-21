1efd4c75933facf244b2d2ff5a8b9dd28cef3b25
Vladimir Rybas <vladimirrybas@gmail.com>
2014-03-21 09:19:12 +0700

    ruby: use "spring" in ZSH aliases for cucumber/rspec

--- ruby/aliases.zsh
+++ ruby/aliases.zsh
@@ -8,6 +8,6 @@ alias migrate='rake db:migrate db:test:clone'
 
 alias v='vagrant'
 
-alias cuke='bundle exec cucumber --tags @active  --format progress'
-alias cukes='bundle exec cucumber --format progress --require features'
-alias specs='bundle exec rspec spec/'
+alias cuke='bundle exec spring cucumber --tags @active  --format progress'
+alias cukes='bundle exec spring cucumber --format progress --require features'
+alias specs='bundle exec spring rspec spec/'
