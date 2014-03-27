d35e28b4688ccfc38bf18a0b5f5184a9c9d010db
Vladimir Rybas <vladimirrybas@gmail.com>
2014-03-27 08:19:51 +0700

    ruby: cukes aliases updated. Don't require all features on the run
    If specific feature passed as a param, there's no point to require all
    features. Just the step definitions and env stuff should be required.

--- ruby/aliases.zsh
+++ ruby/aliases.zsh
@@ -9,5 +9,5 @@ alias migrate='rake db:migrate db:test:clone'
 alias v='vagrant'
 
 alias cuke='bundle exec spring cucumber --tags @active'
-alias cukes='bundle exec spring cucumber --require features'
+alias cukes='bundle exec spring cucumber --require features/step_definitions/ features/support/'
 alias specs='bundle exec spring rspec spec/'
