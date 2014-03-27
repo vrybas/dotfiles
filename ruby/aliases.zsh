2342ff5250f1b5d7225b812668551970a853946c
Vladimir Rybas <vladimirrybas@gmail.com>
2014-03-27 08:14:50 +0700

    ruby: cukes aliases updated. --format option removed.
    Progress is a default format now

--- ruby/aliases.zsh
+++ ruby/aliases.zsh
@@ -8,6 +8,6 @@ alias migrate='rake db:migrate db:test:clone'
 
 alias v='vagrant'
 
-alias cuke='bundle exec spring cucumber --tags @active  --format progress'
-alias cukes='bundle exec spring cucumber --format progress --require features'
+alias cuke='bundle exec spring cucumber --tags @active'
+alias cukes='bundle exec spring cucumber --require features'
 alias specs='bundle exec spring rspec spec/'
