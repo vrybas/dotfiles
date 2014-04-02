1e30e9e2eb50a6ba7b2a7e7d2d8bd8e028a6c59a
Vladimir Rybas <vladimirrybas@gmail.com>
2014-04-02 23:35:30 +0700

    Addes home-directory-wide Gemfile, rbenv-gemset & ruby-version
    So, now all non-project specific gems are installed to ~/.gems via
    Bundler.

--- ruby/aliases.zsh
+++ ruby/aliases.zsh
@@ -7,6 +7,7 @@ alias sd='script/destroy'
 alias migrate='rake db:migrate db:test:clone'
 
 alias v='vagrant'
+alias dbundle='BUNDLE_GEMFILE=.Gemfile bundle'
 
 alias cuke='bundle exec spring cucumber --tags @active'
 alias cukes='bundle exec spring cucumber --require features/step_definitions/ features/support/'
