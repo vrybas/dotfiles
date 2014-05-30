50bb06358775898ff33a59c39cb169e3b1a19f02
Vladimir Rybas <vladimirrybas@gmail.com>
2014-05-30 23:54:48 +0700

    Return 'spring' to rspec/cucumber aliases. Add new aliases
    new aliases for Rails server/console/dbconsole

--- ruby/aliases.zsh
+++ ruby/aliases.zsh
@@ -5,6 +5,10 @@ alias migrate='rake db:migrate db:test:clone'
 alias v='vagrant'
 alias dbundle='BUNDLE_GEMFILE=.Gemfile bundle'
 
-alias cuke='bundle exec cucumber --tags @active'
-alias cukes='bundle exec cucumber --format progress --require features/'
-alias specs='bundle exec rspec'
+alias cuke='bundle exec spring cucumber --tags @active'
+alias cukes='bundle exec spring cucumber --format progress --require features/'
+alias specs='bundle exec spring rspec'
+
+alias server='bundle exec spring rails server'
+alias console='bundle exec spring rails console'
+alias dbconsole='bundle exec spring rails dbconsole'
