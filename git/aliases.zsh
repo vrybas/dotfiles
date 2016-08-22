4755303f8a197f9d05ebdc13709c4d62f8c33e77
Vladimir Rybas <vladimirrybas@gmail.com>
2016-08-22 09:40:44 +0700

    Add git alias WARNING_git_push_using_force

--- git/aliases.zsh
+++ git/aliases.zsh
@@ -11,6 +11,7 @@ alias gl='git pull --prune'
 alias gu='git-upd'
 alias glog="git log --graph --pretty=format:'%Cred%h%Creset %an: %s - %Creset %C(yellow)%d%Creset %Cgreen(%cr)%Creset' --abbrev-commit --date=relative"
 alias gp='git push origin HEAD'
+alias WARNING_git_push_using_force='git push --force origin $(git symbolic-ref --short -q HEAD)'
 alias gd='git diff'
 alias gc='git commit'
 alias gca='git commit -a'
@@ -19,6 +20,6 @@ alias gb='git branch'
 alias gs='git status -sb' # upgrade your git if -sb breaks for you. it's fun.
 alias grm="git status | grep deleted | awk '{\$1=\$2=\"\"; print \$0}' | \
            sed 's/^[ \t]*//' | sed 's/ /\\\\ /g' | xargs git rm"
-alias sq='git-squish'
+alias sq='git-squash'
 alias hard='git reset --hard'
 alias prev='git reset --hard HEAD^1'
