0825ceeb4d60bb1acfe66cbf94435bc1ee688660
Vladimir Rybas <vladimirrybas@gmail.com>
2017-06-01 13:39:57 +0700

    Ruby: Add "coverage" aliases

--- ruby/aliases.zsh
+++ ruby/aliases.zsh
@@ -9,6 +9,8 @@ alias cuke='bundle exec spring cucumber --tags @active'
 alias cukes='bundle exec spring cucumber --format progress --require features/'
 alias spec='bundle exec spring rspec'
 alias specd='bundle exec spring rspec --format=documentation'
+alias cover='COVERAGE=true bundle exec rspec spec && open coverage/index.html'
+alias coverd='COVERAGE=true bundle exec rspec spec --format=documentation && open coverage/index.html'
 
 alias server='bundle exec spring rails server'
 alias console='bundle exec spring rails console'
