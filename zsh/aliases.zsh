bed779cfefff22675fa888e8a487b18dce41dce1
Vladimir Rybas <vladimirrybas@gmail.com>
2017-06-01 13:40:54 +0700

    Zsh: add alias for printing utc timestamp

--- zsh/aliases.zsh
+++ zsh/aliases.zsh
@@ -1,5 +1,6 @@
 alias reload!='. ~/.zshrc'
 alias utc="ruby -e 'puts Time.now.utc'"
+alias utcn="ruby -e \"puts Time.now.utc.strftime('%Y%m%d%H%M%S')\""
 alias l="clear"
 alias x="exit"
 alias i="imgur2"
